d1795768b0c0685b172114acdc85e9c000183226
xxcandiez <josephdong1998@gmail.com>
2018-11-25 02:55:21 -0500

    completed tests for point class

--- tests/point-tests.py
+++ tests/point-tests.py
@@ -1,4 +1,6 @@
 import sys
+import lxml.etree as ET
+
 sys.path.insert(0, '../objects')
 
 import unittest
@@ -12,13 +14,19 @@ class SimpleTests(unittest.TestCase):
 
     def testXy(self):
         self.assertIs(type(self.point.x), int)
-        self.assertIs(type(self.point.y), self.y)
+        self.assertIs(type(self.point.y), int)
 
         self.assertEqual(self.point.x, self.x)
         self.assertEqual(self.point.y, self.y)
 
     def testStyle(self):
         self.assertIs(type(self.point.style), dict)
+        self.assertEqual(self.point.style['cx'], str(self.x))
+        self.assertEqual(self.point.style['cy'], str(self.y))
+
+    def testNode(self):
+        self.assertIs(type(self.point.node), ET._Element)
+
 
 if __name__ == '__main__':
     unittest.main()
