7aaecf2b8f24763a5a51469af6c967098eb2623c
xxcandiez <josephdong1998@gmail.com>
2019-01-18 20:56:26 -0500

    started working on numberline forgot to branch first

--- objects/graphing/NumberLine.py
+++ objects/graphing/NumberLine.py
@@ -3,3 +3,17 @@ import geometry.Point as Point
 import compound.LabeledLine as LabeledLine
 
 class NumberLine():
+    def __init__():
+        """
+        :param config:
+
+        :config value:
+        :config xmin:
+        :config xmax:
+        :config dot:
+        :config arrow:
+        :config marks:
+        """
+        return None
+
+    @staticmethod
